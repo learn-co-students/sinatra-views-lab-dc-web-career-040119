f912607a1dd9a6d074b3515deba46ed488b9f586
errolgh <ewatsonrei@gmail.com>
2019-04-24 09:01:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 class App < Sinatra::Base
-
+#leads to the homepage
 	get '/' do
 		erb :index
 	end
+#sends a get request to the hello page
+	get '/hello' do
+		erb :hello
+	end
+#get request to bye age
+	get '/goodbye' do
+		erb :goodbye
+	end
 
-
+	get '/date' do
+		erb :date
+	end
 end
